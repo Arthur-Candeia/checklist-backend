09167512b6149c5d215dd553a51b669c50645af2
Arthur-Candeia <arthurheher@hotmail.com>
2023-06-12 00:09:14 -0300

    mudanca no Schema para trabalhar com usuarios

--- backend/db/model.js
+++ backend/db/model.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const taskSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   name: {type: String, required: true},
-  done: {type: Boolean, default: false}
+  password: {type: String, required: true},
+  tasks: [
+    {
+      content: {type: String, required: true},
+      done: {type: Boolean, default: false}
+    }
+  ]
 })
 
-module.exports = mongoose.model('Task', taskSchema)
+module.exports = mongoose.model('User', userSchema)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Task = require('../db/model')
+const User = require('../db/model')
 
 router.get('/', async (request, response) => {
-  let task = new Task({name: 'nome'})
-  await task.save()
-  let passar = await Task.find()
-  response.status(200).send(`<h1>${passar}</h1>`)
+  //let user = new User({name: 'nome', password: '1234', tasks: {content: 'conteúdo'}})
+  //await user.save()
+  let passar = await User.find()
+  response.status(200).send(`<h3>${passar}</h3>`)
+})
+
+router.get('/add', async (request, response) => {
+  //let user = await User.findById('648689b0316798868667d033')
+  //await user.tasks.push({content: 'TERCEIRO CONTEÚDO'})
+  //await user.save()
+  let passar = await User.findById('648689b0316798868667d033')
+  response.status(200).send(`<h3>${passar}</h3>`)
 })
 
 module.exports = router
