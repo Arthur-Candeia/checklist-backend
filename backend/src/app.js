676ca00f3f79e5be2ae6c19328aacf8f96b5f8e0
Arthur-Candeia <arthurheher@hotmail.com>
2023-06-16 23:33:50 -0300

    tentativa uso cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,12 @@ const methodOverride = require('method-override')
 const router = require('./routes')
 require('../db/db')
 
-app.use(cors({
-  origin: '*'
-}))
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method', {methods: ['POST', 'GET']}))
