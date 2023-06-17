83f4174b35ee9f6985e5003b77fd2f856b58e2e5
Arthur-Candeia <arthurheher@hotmail.com>
2023-06-16 23:20:55 -0300

    mudanca na resposta quando o done sofre atualizacao

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,16 +5,13 @@ const methodOverride = require('method-override')
 const router = require('./routes')
 require('../db/db')
 
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json())
-app.use(cors())
 app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method', {methods: ['POST', 'GET', 'PUT', 'DELETE']}))
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}))
+
 
 app.use('/', router)
 
